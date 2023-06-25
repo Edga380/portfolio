6c41259d7abb8efa4301ebe607447afb1a68cfec
Edga380 <a.edgaras44@gmail.com>
2023-06-25 18:09:18 +0100

    Projects slideshow

---
+++ javascript/slideShow.js
@@ -0,0 +1,51 @@
+// Get all project buttons and projects
+const projectButtons = document.querySelectorAll('[id^="projectButton"]');
+const projects = document.querySelectorAll('.project');
+let slideShowTimeOut = 0;
+var num = 0;
+
+// Function to handle button click
+function handleButtonClick(projectIndex) {
+  projects.forEach((project, index) => {
+    project.style.display = index === projectIndex ? "flex" : "none";
+  });
+
+  projectButtons.forEach((button, index) => {
+    button.style.backgroundColor = index === projectIndex ? "rgba(0, 93, 153, 1)" : "";
+  });
+}
+
+// Add event listeners to project buttons
+projectButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    handleButtonClick(index);
+    num = index;
+  });
+});
+
+// Add event listeners to project buttons
+projects.forEach((project) => {
+    project.addEventListener("mouseover", () => {
+    clearTimeout(slideShowTimeOut);
+  });
+});
+
+// Add event listeners to project buttons
+projects.forEach((project) => {
+    project.addEventListener("mouseout", () => {
+        slideShowTimeOut = setTimeout(SlideShow, 1000);
+  });
+});
+
+// Automatically run through projects
+SlideShow();
+function SlideShow(){
+    handleButtonClick(num);
+    if(projects.length - 1 <= num){
+        num = 0;
+    }
+    else{
+        num++;
+    }
+    slideShowTimeOut = setTimeout(SlideShow, 3000);
+};
